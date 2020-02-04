af9de0c1094ffa813c3a9f158aa34ad03bd0b1b3
Pledge Davis <pledgedavis14@gmail.com>
2020-02-04 22:19:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,8 +23,6 @@ class Scraper
     page = Nokogiri::HTML(open(profile_url))
      student = {}
 
-     # student[:profile_quote] = page.css(".profile-quote")
-     # student[:bio] = page.css("div.description-holder p")
      container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
      container.each do |link|
        if link.include?("twitter")
@@ -40,7 +38,7 @@ class Scraper
      student[:profile_quote] = page.css(".profile-quote").text
      student[:bio] = page.css("div.description-holder p").text
      student
- 
+
   end
 
 end
